0026f39e68058eab2821ddda00c2349bc7a1a1d7
mueller04 <mikemueller04@hotmail.com>
2015-06-30 18:32:05 -0400

    CardAddSavesCorrectAnswertoCollection and CardAddSavesInCorrectAnswer2toCollection passed

--- TestCards.View/Tests/AddCardClickTests.cs
+++ TestCards.View/Tests/AddCardClickTests.cs
@@ -22,7 +22,34 @@ namespace TestCards.View.Tests
 
             //Assert
             Assert.AreEqual(cardRepo.Cards[0].Question, "What are Dolphins");
+        }
 
+        [Test]
+        public void CardAddSavesCorrectAnswertoCollection()
+        {
+            //Arrange
+            CardRepository cardRepo = new CardRepository();
+
+            //Act
+            cardRepo.Add("What are Dolphins", "They are Mammals", "They are fish", "They are Godzilla", "Aliens");
+
+            //Assert
+            Assert.AreEqual(cardRepo.Cards[0].CorrectAnswer.Text, "They are Mammals");
+            Assert.AreEqual(cardRepo.Cards[0].CorrectAnswer.IsCorrect, true);
+        }
+
+        [Test]
+        public void CardAddSavesInCorrectAnswer2toCollection()
+        {
+            //Arrange
+            CardRepository cardRepo = new CardRepository();
+
+            //Act
+            cardRepo.Add("What are Dolphins", "They are Mammals", "They are fish", "They are Godzilla", "Aliens");
+
+            //Assert
+            Assert.AreEqual(cardRepo.Cards[0].IncorrectAnswer2.Text, "They are Godzilla");
+            Assert.AreEqual(cardRepo.Cards[0].IncorrectAnswer2.IsCorrect, false);
         }
 
     }
