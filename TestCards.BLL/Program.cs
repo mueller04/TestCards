3203f4a558d3c4788d200eddefef81316b31dd83
mueller04 <mikemueller04@hotmail.com>
2015-06-30 13:51:41 -0400

    init

---
+++ TestCards.BLL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCards.BLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HelloWorld
+        }
+    }
+}
